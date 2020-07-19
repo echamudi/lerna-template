b2be2538cdab94ab4916fc2477d6754a7ebd0052
Ezzat Chamudi <echamudi@outlook.com>
2020-07-19 19:42:01 +0300

    Remove minimath

--- packages/flip-string/index.js
+++ packages/flip-string/index.js
@@ -1,6 +1,5 @@
 var arrayToString = require('@echamudi/array-to-string');
 var stringToArray = require('@echamudi/string-to-array');
-var minimath = require('@echamudi/minimath');
 
 module.exports = function (x) {
     return arrayToString(stringToArray(x).reverse());
